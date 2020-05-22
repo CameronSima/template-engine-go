e24397662a4c555e11be0b327bb637fabb47e8c4
cameron <cjsima@gmail.com>
2020-05-22 15:57:47 -0400

    ...

--- base/base.py
+++ base/base.py
@@ -25,6 +25,7 @@ class Template:
         self.python_funcs = self.get_python_funcs()
 
     def render(self, context, request):
+        self.original_context = context
         context = self.prepare_context(context, request)
         result = lib.render(
             self.template_name.encode('utf-8'),
@@ -58,4 +59,12 @@ class Template:
 
     def prepare_context(self, context, request=None):
         func_names = {funcName: 1 for funcName, _ in self.python_funcs.items()}
+        context = {k: v for k, v in context.items() if is_serializable(v)}
         return json.dumps({**context, **func_names})
+
+def is_serializable(data):
+    try:
+        json.dumps(data)
+        return True
+    except (TypeError, OverflowError):
+        return False
--- base/test_base.py
+++ base/test_base.py
@@ -12,7 +12,7 @@ class TestBase(unittest.TestCase):
         r = t.render(c, None)
         self.assertEqual(' '.join(r.split()), '<div class="main"> <h1>Hello world</h1> </div>')
 
-    def test_post_process(self):
+    def test_post_process_python_func(self):
         c = {'param1': 'cameron'}
 
         p = Phantom({})
@@ -22,7 +22,21 @@ class TestBase(unittest.TestCase):
         print(r)
         print(t)
         self.assertEqual(' '.join(r.split()), '<div> cameronparam2 </div>')
-    
+
+    def test_post_process_callable_in_context(self):
+        class TestObject:
+            @property
+            def callable_property(self):
+                return 'callable_value'
+
+        c = {'callable': TestObject()}
+        p = Phantom({})
+        p.app_dirname = './'
+        t = p.get_template('test_post_process_callable.html')
+        r = t.render(c, None)
+        print(r)
+        print(t)
+        self.assertEqual(' '.join(r.split()), '<div> callable_value </div>')
 
 
 if __name__ == '__main__':
